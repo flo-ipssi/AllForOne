8c97945ec86c49a2ec9657b7cfbf6a9bf0b67541
vincentcrb <vincent.crb@gmail.com>
2019-06-17 11:49:04 +0200

    Première phase vue profil

--- components/Profil.js
+++ components/Profil.js
@@ -1,11 +1,32 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, ImageBackground } from 'react-native';
+import { Icon } from 'react-native-elements'
 
 export default class Profil extends React.Component {
   render() {
     return (
+      <View>
+
+      <View style={styles.settings}>
+      <Icon name='settings'
+            color='#333'
+      />
+
+      </View>
+
       <View style={styles.container}>
-        <Text>Profil</Text>
+      <ImageBackground source={require('../assets/workout.jpg')} style={ styles.image } imageStyle={{ borderRadius: 38 }}>
+        
+      </ImageBackground>
+
+      <Text style={styles.surname}>All might</Text>
+
+      </View>
+
+      <View style={styles.statsContainer}>
+
+      <Text style={styles.title}>Mes stats</Text>
+      </View>
       </View>
     );
   }
@@ -16,5 +37,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 24,
+    borderColor: '#ccc',
+    borderBottomWidth: 0.2,
+   
   },
+  settings: {
+    position: 'absolute',
+    right: 10,
+    marginTop: 10,
+  },
+  image: {
+    height: 75,
+    borderRadius: 38,
+    width: 75,
+    marginBottom: 10
+  },
+  surname: {
+    fontWeight: '500',
+  },
+  title: {
+    padding
+    : 10,
+    fontWeight: 'bold',
+    fontSize: 20,
+  }
 });
