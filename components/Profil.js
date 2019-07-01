ba540409f9d0e6cff47fed29d035c1a5f15c6d96
vincentcrb <vincent.crb@gmail.com>
2019-07-01 15:09:19 +0200

    Ajout Chart dans le profil

--- components/Profil.js
+++ components/Profil.js
@@ -1,8 +1,24 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, Image, ImageBackground } from 'react-native';
+import { Text, View, StyleSheet, Image, ImageBackground, Dimensions } from 'react-native';
 //import { Icon } from 'react-native-elements'
 import Icon from '@expo/vector-icons/MaterialIcons';
 
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from 'react-native-chart-kit'
+
+const data = [
+  { name: 'Seoul', population: 21500000, color: 'rgba(131, 167, 234, 1)', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Toronto', population: 2800000, color: '#F00', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Beijing', population: 527612, color: 'red', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'New York', population: 8538000, color: '#ffffff', legendFontColor: '#7F7F7F', legendFontSize: 15 },
+  { name: 'Moscow', population: 11920000, color: 'rgb(0, 0, 255)', legendFontColor: '#7F7F7F', legendFontSize: 15 }
+]
 
 
 export default class Profil extends React.Component {
@@ -34,6 +50,42 @@ export default class Profil extends React.Component {
 
       <Text style={styles.title}>Mes stats</Text>
       </View>
+      <View>
+
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [{
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }]
+          }}
+          width={Dimensions.get('window').width-20} // from react-native
+          height={220}
+          chartConfig={{
+            backgroundColor: '#2B83ED',
+            backgroundGradientFrom: '#0A6CE3',
+            backgroundGradientTo: '#4E96EC',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16
+            }
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+            marginHorizontal: 10,
+          }}
+        />
+</View>
       </View>
     );
   }
